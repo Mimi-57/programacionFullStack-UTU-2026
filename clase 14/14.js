a0d324a4c1ddb6bd0deccb656ad217dd5e985e9f
Mimi-57 <isabelromero1857@gmail.com>
2026-06-10 15:16:21 -0300

    y si me tiro del puente?

---
+++ clase 14/14.js
@@ -0,0 +1,19 @@
+let botonTraerImagen = document.querySelector("#load-cat");
+let imagenGatito = document.querySelector("#gatitos");
+let botonMeGusta = document.querySelector("#like");
+let botonNoMeGusta = document.querySelector("#dislike");
+let contadorMeGusta = document.querySelector(".cont-like");
+let contadorNoMeGusta = document.querySelector(".cont-dislike");
+let mensajeError = document.querySelector(".error");
+
+botonTraerImagen.addEventListener("click", async() => { 
+    let response = await fetch("https://api.thecatapi.com/v1/images/search");
+    let data = await response.json();
+
+    console.log(data);
+
+    let imagen = data[0].url;
+
+    imagenGatito.src = imagen;  
+})
+
