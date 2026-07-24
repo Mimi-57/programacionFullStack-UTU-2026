da53979f94064121365a79f6a96eb208a73fe059
Mimi-57 <isabelromero1857@gmail.com>
2026-07-24 17:59:12 -0300

    Update 1.php

--- clases de PHP/bloque 3/1.php
+++ clases de PHP/bloque 3/1.php
@@ -9,11 +9,11 @@ if ($numero1 == $numero2){
 
 if  ($numero1 > $numero2){
 
-    echo "El primer numero es mayo";
+    echo "El primer numero es mayor";
 }
 
 if  ($numero2 > $numero1){
 
-    echo "El segundo numero es mayo";
+    echo "El segundo numero es mayor";
 }
 ?>
