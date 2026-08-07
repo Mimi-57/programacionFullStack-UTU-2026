8adceddce4407240cfc9a4493b9fcf72f40b1304
Mimi-57 <isabelromero1857@gmail.com>
2026-08-07 18:40:58 -0300

    Veneco puto

---
+++ clases de PHP/bloque 10/1.php
@@ -0,0 +1,10 @@
+<?php
+
+function saludar() {
+    echo "Bienvenido al sistema<br>";
+}
+
+saludar();
+saludar();
+
+?>
---
+++ clases de PHP/bloque 10/2.php
@@ -0,0 +1,11 @@
+<?php
+
+function saludar($nombre){
+    echo "Hola," . $nombre . "<\n> Bienvenido al sistema";
+}
+
+saludar ("Mimi");
+saludar ("Totii");
+saludar ("Cazaputas44");
+
+?>
