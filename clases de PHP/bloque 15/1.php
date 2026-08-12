f5b6c9493f4530a54bf5fd1342c6e11ca9f69c04
Mimi-57 <isabelromero1857@gmail.com>
2026-08-12 16:00:05 -0300

    el panadero con el pan

---
+++ clases de PHP/bloque 15/1.php
@@ -0,0 +1,10 @@
+<?php
+$persona = [
+    "nombre" => "Chanchito feliz",
+    "edad" => 22,
+    "ciudad" => "san josé"
+];
+
+echo "Mi nombre es" . $persona["nombre"] . ", tengo" . $persona["edad"] . " años y vivo en " . $persona["ciudad"] . ".";
+
+?>
